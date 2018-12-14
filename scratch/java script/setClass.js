e469c31f1b7ab9e3dd77ebc380ed3b8fb519f002
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2018-12-14 16:39:29 +0100

    java script

--- scratch/java script/setClass.js
+++ scratch/java script/setClass.js
@@ -1,20 +1,10 @@
 console.log('My code is running!');
+
 function greet(name) {
   console.log('Hello, ' + name);
-  }
+}
 
-  function setClassTip(event) {
-  let el = document.getElementById('mypara');
-  el.className = 'tip';
-
-  }
-  function setClassWarning(event) {
-  let el = document.getElementById('mypara');
-  el.className = 'warning';
-
-  }
-  function clearClass(event) {
-  let el = document.getElementById('mypara');
-  el.className = '';
-
-  }
+function setClass(event, className){
+    let el =document.getElementById('mypara')
+    el.className = className;
+}
