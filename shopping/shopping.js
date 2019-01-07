0c6786ac08552079a11b9e86416ea424eb459103
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2019-01-07 14:38:59 +0100

    commit the value

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function(event){
     document.querySelector('button').addEventListener('click',function(event) {
         console.log('The button was clicked');
         let el=document.getElementById('item');
-        el.value;
-console.log(el.value);
+        console.log(el.value);
 
     });
 });
