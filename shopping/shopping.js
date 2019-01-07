3c0eb386060445cd687c29f0563d4ced1d98bf1d
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2019-01-07 11:58:58 +0100

    buttons clicks

---
+++ shopping/shopping.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(event){
+    document.querySelector('button').addEventListener('click',function(event) {
+        console.log('The button was clicked');
+    });
+});
