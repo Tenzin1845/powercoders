0334b007316c6ca4da0f3f4f862be15a9ef3f6f4
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2019-01-07 13:50:18 +0100

    input box value

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(event){
     document.querySelector('button').addEventListener('click',function(event) {
         console.log('The button was clicked');
+        let el=document.getElementById('item');
+        el.value;
+console.log(el.value);
+
     });
 });
