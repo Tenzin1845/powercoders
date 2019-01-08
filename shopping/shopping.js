d74a0bcd22ed36288b5fb9828a1b6580f9bb0702
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2019-01-08 16:12:26 +0100

    final commit

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,8 +1,33 @@
-document.addEventListener('DOMContentLoaded', function(event){
-    document.querySelector('button').addEventListener('click',function(event) {
-        console.log('The button was clicked');
-        let el=document.getElementById('item');
-        console.log(el.value);
+function addItem(itemName) {
+  let li = document.createElement('li');
+  let span = document.createElement('span');
+  span.innerText = itemName;
+  li.appendChild(span);
+  let button = document.createElement('button');
+  button.innerText = 'delete';
 
-    });
+  button.addEventListener('click', function (event) {
+    console.log('delete button clicked: ' + itemName);
+    li.remove();
+  });
+
+  li.appendChild(button);
+
+  return li;
+
+}
+
+    document.addEventListener('DOMContentLoaded', function (event);
+    document.querySelector('button').addEventListener('click', function (event) {
+    console.log('The button was clicked');
+    let el = document.getElementById('item');
+    console.log(el.value);
+    let li = document.createElement('li');
+    li = addItem(el.value);
+    let ul = document.querySelector('ul');
+
+    ul.appendChild(li);
+
+
+  });
 });
