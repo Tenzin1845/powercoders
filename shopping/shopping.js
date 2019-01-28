854905749192e4eb277b08c845b3684d521fc8b6
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2019-01-28 10:58:23 +0100

    new update

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -11,11 +11,15 @@ function addItem(itemName) {
   span.innerText = itemName;
   li.appendChild(span);
   const button = document.createElement('button');
-  button.innerText = 'delete';
+  button.className = 'delete';
+  li.appendChild(button);
 
   button.addEventListener('click', function (event) {
     li.remove();
     document.getElementById('item').focus();
+
+    document.querySelector('button#clear').disabled =
+        document.querySelectorAll('li').length === 0;
 
   });
 
@@ -28,6 +32,10 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const inputbox = document.getElementById('item');
   const shoppingList = document.querySelector('ul');
   const addItemButton = document.querySelector('button');
+  const clearAll = document.getElementById('deleteAll');
+  clearAll.disabled = true;
+
+
 
 
 
@@ -40,13 +48,24 @@ document.addEventListener('DOMContentLoaded', function (event) {
     shoppingList.appendChild(addItem(trimmedValue));
       inputbox.value = '';
       addItemButton.disabled = true;
+      clearAll.disabled =false;
       inputbox.focus();
 
+  });
+
+  clearAll.addEventListener('click', function () {
+    let listItems = document.querySelectorAll('li');
+    listItems.forEach(function (el) {
+      el.remove();
+    });
+    clearAll.disabled = true;
+    inputBox.focus();
   });
 
   inputbox.addEventListener('keyup', function (event) {
     const trimmedValue = inputbox.value.trim();
     addItemButton.disabled = trimmedValue === '';
+    clearAll.disabled = false;
 
     if (trimmedValue === '') {
       return;
@@ -60,11 +79,13 @@ document.addEventListener('DOMContentLoaded', function (event) {
     shoppingList.appendChild(addItem(trimmedValue));
   inputbox.value = '';
   addItemButton.disabled = true;
+  clearAll.disabled = false;
 
   });
 
   inputbox.focus();
   addItemButton.disabled = true;
+  clearAll.disabled = true;
 });
 
 
