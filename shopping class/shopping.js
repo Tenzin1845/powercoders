b1da78e2b1d738a5331949319762fcb88107d847
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2019-02-04 11:40:10 +0100

    using new style classes in coding

---
+++ shopping class/shopping.js
@@ -0,0 +1,144 @@
+
+class ShoppingListItem {
+
+  /**
+   *
+   * @param {string} name
+   * @param {string} quantity
+   */
+  constructor(name, quantity) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+
+   toListItem (){
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerText = this.name;
+    li.appendChild(span);
+
+
+
+    if (this.quantity !== '') {
+      const quantitySpan = document.createElement('span');
+      quantitySpan.innerText = `(${this.quantity})`;
+      li.appendChild(quantitySpan);
+    }
+
+
+    const button = document.createElement('button');
+    button.className = 'delete';
+    li.appendChild(button);
+
+    button.addEventListener('click', function () {
+      li.remove();
+
+
+      document.getElementById('item').focus();
+
+      document.querySelector('button#deleteAll').disabled =
+          document.querySelectorAll('li').length === 0;
+
+    });
+
+    li.appendChild(button);
+    return li;
+
+  }
+
+}
+
+
+function domContentLoaded() {
+  const inputbox = document.getElementById('item');
+  const shoppingList = document.querySelector('ul');
+  const addItemButton = document.querySelector('button');
+  const clearAll = document.getElementById('deleteAll');
+  const quantity = document.getElementById('quantity');
+  clearAll.disabled = true;
+
+
+  addItemButton.addEventListener('click', function () {
+    const trimmedValue = inputbox.value.trim();
+    const quantity = document.getElementById('quantity');
+    if (trimmedValue === '') {
+      return;
+    }
+    const item = new ShoppingListItem(trimmedValue,quantity.value.trim());
+    shoppingList.appendChild(item.toListItem());
+
+    inputbox.value = '';
+    quantity.value = '';
+    addItemButton.disabled = true;
+    clearAll.disabled = false;
+    inputbox.focus();
+
+  });
+
+  clearAll.addEventListener('click', function () {
+    let listItems = document.querySelectorAll('li');
+    listItems.forEach(function (el) {
+      el.remove();
+    });
+    clearAll.disabled = true;
+    inputbox.focus();
+  });
+
+  inputbox.addEventListener('keyup', function () {
+    const trimmedValue = inputbox.value.trim();
+    const quantity = document.getElementById('quantity');
+    addItemButton.disabled = trimmedValue === '';
+    if (trimmedValue === '') {
+      return;
+    }
+
+    if (event.key !== 'Enter') {
+      return;
+    }
+    const item = new ShoppingListItem(trimmedValue,quantity.value.trim());
+    shoppingList.appendChild(item.toListItem());
+    inputbox.value = '';
+    quantity.value = '';
+    addItemButton.disabled = true;
+    clearAll.disabled = false;
+
+  });
+
+  quantity.addEventListener('keyup', function () {
+    const trimmedValue = inputbox.value.trim();
+    const quantity = document.getElementById('quantity');
+    addItemButton.disabled = trimmedValue === '';
+
+    if (trimmedValue === '') {
+      return;
+    }
+
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    const item = new ShoppingListItem(trimmedValue,quantity.value.trim());
+    shoppingList.appendChild(item.toListItem());
+
+    inputbox.value = '';
+    quantity.value = '';
+    addItemButton.disabled = true;
+    clearAll.disabled = false;
+
+  });
+
+  inputbox.focus();
+  addItemButton.disabled = true;
+  clearAll.disabled = true;
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', function () {
+    domContentLoaded();
+  });
+} else {
+  domContentLoaded();
+
+}
+
+
