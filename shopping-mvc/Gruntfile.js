524b0bed5d8720e2ed167d3c8c8182b320476a2e
Tenzin Osel <tenzin.tashitsang@powercoders.org>
2019-02-14 15:46:51 +0100

    updating the grundjs

--- shopping-mvc/Gruntfile.js
+++ shopping-mvc/Gruntfile.js
@@ -2,9 +2,23 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    less: {
+      build: {
+        files: {
+          'src/css/style.css': 'src/css/style.less'
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'build/css/style.min.css': ['src/css/*.css']
+        }
+      }
+    },
     uglify: {
       options: {
-        banner: '/! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> /\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         src: ['src/js/*.js', '!src/js/ui.js', 'src/js/ui.js'],
@@ -25,20 +39,39 @@ module.exports = function(grunt) {
         }]
       }
     },
-    cssmin: {
-      target: {
-        files: {
-          'build/css/style.min.css': ['src/css/*.css']
-        }
+    watch: {
+      less: {
+        files: 'src/css/*.less',
+        tasks: ['less']
+      },
+      css: {
+        files: 'src/css/*.css',
+        tasks: ['cssmin']
+      },
+      scripts: {
+        files: 'src/js/*.js',
+        tasks: ['uglify']
+      },
+      html: {
+        files: 'src/html/index.prod.html',
+        tasks: ['copy:html']
+      },
+      static: {
+        files: 'src/static/*.*',
+        tasks: ['copy:static']
       }
     }
   });
+
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  // Default task(s).
-  grunt.registerTask('default', ['cssmin','uglify', 'copy']);
-};
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
+
+  // Default task(s).
+  grunt.registerTask('default', ['less','cssmin','uglify', 'copy']);
+};
 
